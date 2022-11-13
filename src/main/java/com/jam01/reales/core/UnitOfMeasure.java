a04df2ebe7cd33ce067e3671d1cfcafc20dc9e10
jam01 <jam01@protonmail.com>
2022-11-13 01:23:45 -0600

    add value pattern

---
+++ src/main/java/com/jam01/reales/core/UnitOfMeasure.java
@@ -0,0 +1,5 @@
+package com.jam01.reales.core;
+
+public record UnitOfMeasure(String Name, String symbol) {
+
+}
---
+++ src/main/java/com/jam01/reales/core/Value.java
@@ -0,0 +1,9 @@
+package com.jam01.reales.core;
+
+import java.math.BigDecimal;
+
+public class Value {
+    public BigDecimal value;
+    public UnitOfMeasure unit;
+
+}
